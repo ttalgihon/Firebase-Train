03b848a789cb73fc78793cdb6762828993db8bcd
ttalgihon <vivithefool@gmail.com>
2019-03-26 16:11:21 -0400

    moved tokens into a config file

--- firebase.js
+++ firebase.js
@@ -1,17 +1,16 @@
-{/* <script src = "https://www.gstatic.com/firebasejs/5.9.1/firebase.js"></script> */}
-
 // Initialize Firebase
 let config = {
-    apiKey: "AIzaSyAKbZge9Fv_pjpRiTWS8rP-KJhuLUggAIU",
-    authDomain: "fir-train-fb7a6.firebaseapp.com",
-    databaseURL: "https://fir-train-fb7a6.firebaseio.com",
-    projectId: "fir-train-fb7a6",
-    storageBucket: "fir-train-fb7a6.appspot.com",
-    messagingSenderId: "529292382408"
+    apiKey: APIKEY,
+    authDomain: AUTHDOMAIN,
+    databaseURL: DATABASEURL,
+    projectId: PROJECTID,
+    storageBucket: STORAGE,
+    messagingSenderId: MESSAGINGSENDERID
 };
 // Initialize the default app
 let defaultApp = firebase.initializeApp(config);
 console.log(defaultApp); //Check chrome inspector
 
+// Firebase Realtime Database (through the default app)
 let defaultDatabase = defaultApp.database();
-console.log(defaultDatabase); //works
+console.log(defaultDatabase); //Firebase works!
