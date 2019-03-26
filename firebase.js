4df1a5d5af8ce96f55e44515d102449ff08c69e6
ttalgihon <vivithefool@gmail.com>
2019-03-26 13:53:20 -0400

    create a js file and path

---
+++ firebase.js
@@ -0,0 +1,12 @@
+<script src = "https://www.gstatic.com/firebasejs/5.9.1/firebase.js"></script>
+
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyAKbZge9Fv_pjpRiTWS8rP-KJhuLUggAIU",
+    authDomain: "fir-train-fb7a6.firebaseapp.com",
+    databaseURL: "https://fir-train-fb7a6.firebaseio.com",
+    projectId: "fir-train-fb7a6",
+    storageBucket: "fir-train-fb7a6.appspot.com",
+    messagingSenderId: "529292382408"
+};
+firebase.initializeApp(config);
