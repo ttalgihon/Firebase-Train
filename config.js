875aab4146509f051a12716ee9cb58023912a0b9
ttalgihon <31228739+ttalgihon@users.noreply.github.com>
2019-03-26 18:43:36 -0400

    delete off github

--- config.js
+++
@@ -1,7 +0,0 @@
-// This holds the TOKENS to connect to the firebase
-const APIKEY = "AIzaSyAKbZge9Fv_pjpRiTWS8rP-KJhuLUggAIU";
-const AUTHDOMAIN = "fir-train-fb7a6.firebaseapp.com";
-const DATABASEURL = "https://fir-train-fb7a6.firebaseio.com";
-const PROJECTID = "fir-train-fb7a6";
-const STORAGE = "fir-train-fb7a6.appspot.com";
-const MESSAGINGSENDERID = "529292382408";
