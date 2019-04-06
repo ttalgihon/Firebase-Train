99bfccc5dc3da87de88356f961f60d99a115aec6
ttalgihon <vivithefool@gmail.com>
2019-04-05 21:33:40 -0400

    finished with momentjs

--- main.js
+++ main.js
@@ -1,149 +1,99 @@
 // Initialize Firebase
 let config = {
-  apiKey: process.env.APIKEY,
-  authDomain: process.env.AUTHDOMAIN,
-  databaseURL: process.env.DATABASEURL,
-  projectId: process.env.PROJECTID,
-  storageBucket: process.env.STORAGE,
-  messagingSenderId: process.env.MESSAGINGSENDERID
+  apiKey: "AIzaSyAKbZge9Fv_pjpRiTWS8rP-KJhuLUggAIU",
+  authDomain: "fir-train-fb7a6.firebaseapp.com",
+  databaseURL: "https://fir-train-fb7a6.firebaseio.com",
+  projectId: "fir-train-fb7a6",
+  storageBucket: "fir-train-fb7a6.appspot.com",
+  messagingSenderId: "529292382408"
 };
-// Initialize the default app
 let defaultApp = firebase.initializeApp(config);
-console.log(defaultApp); //Check chrome inspector
 
 // Firebase Realtime Database (through the default app)
 let defaultDatabase = defaultApp.database();
-console.log(defaultDatabase); //Firebase works!
 
-  let trainData = firebase.database();
+// 3. Button for adding trains
+$("#add-train-btn").on("click", function (event) {
+  // Prevents the default form submit behavior
+  event.preventDefault();
 
-  
-  
-  // 2. Populate Firebase Database with initial data (in this case, I did this via Firebase GUI)
-  // 3. Button for adding trains
-  $("#add-train-btn").on("click", function(event) {
-    // Prevent the default form submit behavior
-    event.preventDefault();
-  
-    // Grabs user input
-    var trainName = $("#train-name-input")
-      .val()
-      .trim();
-    var destination = $("#destination-input")
-      .val()
-      .trim();
-    var firstTrain = $("#first-train-input")
-      .val()
-      .trim();
-    var frequency = $("#frequency-input")
-      .val()
-      .trim();
-  
-    // Creates local "temporary" object for holding train data
-    var newTrain = {
-      name: trainName,
-      destination: destination,
-      firstTrain: firstTrain,
-      frequency: frequency
-    };
-  
-    // Uploads train data to the database
-    trainData.ref().push(newTrain);
-  
-    // Logs everything to console
-    console.log(newTrain.name);
-    console.log(newTrain.destination);
-    console.log(newTrain.firstTrain);
-    console.log(newTrain.frequency);
-  
-    // Alert
-    alert("Train successfully added");
-  
-    // Clears all of the text-boxes
-    $("#train-name-input").val("");
-    $("#destination-input").val("");
-    $("#first-train-input").val("");
-    $("#frequency-input").val("");
-  });
-  
-  // 4. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
-  trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
-    console.log(childSnapshot.val());
-  
-    // Store everything into a variable.
-    var tName = childSnapshot.val().name;
-    var tDestination = childSnapshot.val().destination;
-    var tFrequency = childSnapshot.val().frequency;
-    var tFirstTrain = childSnapshot.val().firstTrain;
-  
-    var timeArr = tFirstTrain.split(":");
-    var trainTime = moment()
-      .hours(timeArr[0])
-      .minutes(timeArr[1]);
-    var maxMoment = moment.max(moment(), trainTime);
-    var tMinutes;
-    var tArrival;
-  
-    // If the first train is later than the current time, sent arrival to the first train time
-    if (maxMoment === trainTime) {
-      tArrival = trainTime.format("hh:mm A");
-      tMinutes = trainTime.diff(moment(), "minutes");
-    } else {
-      // Calculate the minutes until arrival using hardcore math
-      // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
-      // and find the modulus between the difference and the frequency.
-      var differenceTimes = moment().diff(trainTime, "minutes");
-      var tRemainder = differenceTimes % tFrequency;
-      tMinutes = tFrequency - tRemainder;
-      // To calculate the arrival time, add the tMinutes to the current time
-      tArrival = moment()
-        .add(tMinutes, "m")
-        .format("hh:mm A");
-    }
-    console.log("tMinutes:", tMinutes);
-    console.log("tArrival:", tArrival);
-  
-    // Add each train's data into the table
-    $("#train-table > tbody").append(
-      $("<tr>").append(
-        $("<td>").text(tName),
-        $("<td>").text(tDestination),
-        $("<td>").text(tFrequency),
-        $("<td>").text(tArrival),
-        $("<td>").text(tMinutes)
-      )
-    );
-  });
-  
-  // Assume the following situations.
-  
-  // (TEST 1)
-  // First Train of the Day is 3:00 AM
-  // Assume Train comes every 3 minutes.
-  // Assume the current time is 3:16 AM....
-  // What time would the next train be...? ( Let's use our brains first)
-  // It would be 3:18 -- 2 minutes away
-  
-  // (TEST 2)
-  // First Train of the Day is 3:00 AM
-  // Assume Train comes every 7 minutes.
-  // Assume the current time is 3:16 AM....
-  // What time would the next train be...? (Let's use our brains first)
-  // It would be 3:21 -- 5 minutes away
-  
-  // ==========================================================
-  
-  // Solved Mathematically
-  // Test case 1:
-  // 16 - 00 = 16
-  // 16 % 3 = 1 (Modulus is the remainder)
-  // 3 - 1 = 2 minutes away
-  // 2 + 3:16 = 3:18
-  
-  // Solved Mathematically
-  // Test case 2:
-  // 16 - 00 = 16
-  // 16 % 7 = 2 (Modulus is the remainder)
-  // 7 - 2 = 5 minutes away
-  // 5 + 3:16 = 3:21
-  
+  // Grabs user input
+  let trainName = $("#train-name-input")
+    .val()
+    .trim();
+  let destination = $("#destination-input")
+    .val()
+    .trim();
+  let firstTrain = $("#first-train-input")
+    .val()
+    .trim();
+  let frequency = $("#frequency-input")
+    .val()
+    .trim();
+
+  // Creates local "temporary" object for holding train data from user input
+  let tempTrain = {
+    name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: frequency
+  };
+
+  // Confirm Alert
+  if (window.confirm("Your train data will now be added to the database.")) {
+    // Uploads the 'temptrain' object data to the firebase.database
+    defaultDatabase.ref('trains').push(tempTrain);
+  }
+  else {
+    alert("Please submit you data again when you think it's correct.")
+  };
+
+  // Clears all of the text-boxes
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
+
+// 4. Creates a Firebase event for adding trains from the database in a row on the HTML table.
+/*The child_added event is typically used when retrieving a list of items from the database. 
+Unlike value which returns the entire contents of the location, child_added is triggered 
+once for each existing child and then again every time a new child is added to the specified path.*/
+defaultDatabase.ref('trains').on('child_added', function(snapshot, prevChildKey) {
+  const train = snapshot.val();
+  console.log(train)
+
+  // Take the data from the database and seperates it into indiviual variables.
+  let tName = train.name;
+  let tDestination = train.destination;
+  let tFrequency = train.frequency;
+  let tFirstTrain = train.firstTrain;
+
+  let timeArrive = tFirstTrain.split(":"); //takes out :
+  let trainTime = moment().hours(timeArrive[0]).minutes(timeArrive[1]);
+  let maxMoment = moment.max(moment(), trainTime);
+
+  let tMinutesAway;
+  let tArrival;
+  if (maxMoment === trainTime) {
+    tArrival = trainTime.format("hh:mm A");
+    tMinutesAway = trainTime.diff(moment(), "minutes");
+  } else {
+    let differenceTimes = moment().diff(trainTime, "minutes");
+    const tRemainder = differenceTimes % tFrequency;
+    tMinutesAway = tFrequency - tRemainder;
+    // To calculate the arrival time, add the tMinutesAway to the current time
+    tArrival = moment().add(tMinutesAway, "m").format("hh:mm A");
+  }
+
+  // Add each train's data into the table
+  $("#tableID > tbody").append(
+    $("<tr>").append(
+      $("<td>").text(tName),
+      $("<td>").text(tDestination),
+      $("<td>").text(tFrequency),
+      $("<td>").text(tArrival),
+      $("<td>").text(tMinutesAway)
+    )
+  );
+});
